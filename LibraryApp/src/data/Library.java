d6c3c1c663123ecb25b3ec041f006aef0d36faf9
KCisak <krzysiek.cisak@gmail.com>
2017-11-13 18:48:05 +0100

    Added methods toString(), equals() and hashCode()

--- LibraryApp/src/data/Library.java
+++ LibraryApp/src/data/Library.java
@@ -1,35 +1,35 @@
 package data;
-  
+
 public class Library {
-      
+
     public static final int MAX_BOOKS = 1000;
     public static final int MAX_MAGAZINES = 1000;
     private Book[] books;
     private Magazine[] magazines;
     private int booksNumber;
     private int magazinesNumber;
-      
+
     public int getBooksNumber() {
         return booksNumber;
     }
-  
+
     public Book[] getBooks() {
         return books;
     }
-      
+
     public int getMagazinesNumber() {
         return magazinesNumber;
     }
-      
+
     public Magazine[] getMagazines() {
         return magazines;
     }
-  
+
     public Library() {
         books = new Book[MAX_BOOKS];
         magazines = new Magazine[MAX_MAGAZINES];
     }
-      
+
     public void addBook(Book book) {
         if(booksNumber < MAX_BOOKS) {
             books[booksNumber] = book;
@@ -37,9 +37,9 @@ public class Library {
         } else {
             System.out.println("Maxymalna liczba książek została osiągnięta");
         }
-          
+
     }
-      
+
     public void addMagazine(Magazine magazine) {
         if(magazinesNumber < MAX_MAGAZINES) {
             magazines[magazinesNumber] = magazine;
@@ -47,24 +47,24 @@ public class Library {
         } else {
             System.out.println("Maxymalna liczba magazynów została osiągnięta");
         }
-          
+
     }
-      
+
     public void printBooks() {
         if(booksNumber == 0) {
             System.out.println("Brak książek w bibliotece");
         }
         for(int i=0; i<booksNumber; i++) {
-            books[i].printInfo();
+            System.out.println(books[i]);
         }
     }
-      
+
     public void printMagazines() {
         if(magazinesNumber == 0) {
             System.out.println("Brak magazynów w bibliotece");
         }
         for(int i=0; i<magazinesNumber; i++) {
-            magazines[i].printInfo();
+            System.out.println(magazines[i]);
         }
     }
 }
--- LibraryApp/src/data/Magazine.java
+++ LibraryApp/src/data/Magazine.java
@@ -1,10 +1,10 @@
 package data;
- 
+
 public class Magazine extends Publication {
     private int month;
     private int day;
     private String language;
-     
+
     public int getMonth() {
         return month;
     }
@@ -23,17 +23,48 @@ public class Magazine extends Publication {
     public void setLanguage(String language) {
         this.language = language;
     }
- 
+
     public Magazine(String title, String publisher, String language, int year, int month, int day) {
         super(year, title, publisher);
         setLanguage(language);
         setMonth(month);
         setDay(day);
     }
- 
-    public void printInfo() {
-        String info = getTitle() + "; " + getPublisher() + "; " + getYear() + "-"
+
+    @Override
+    public String toString() {
+        return getTitle() + "; " + "; " + getPublisher() + "; " + getYear() + "-"
                 + getMonth() + "-" + getDay() + "; " + getLanguage();
-        System.out.println(info);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + day;
+        result = prime * result + ((language == null) ? 0 : language.hashCode());
+        result = prime * result + month;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Magazine other = (Magazine) obj;
+        if (day != other.day)
+            return false;
+        if (language == null) {
+            if (other.language != null)
+                return false;
+        } else if (!language.equals(other.language))
+            return false;
+        if (month != other.month)
+            return false;
+        return true;
     }
 }
