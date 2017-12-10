bdbe7300203a4bf0924aedb21363551e901d54a5
KCisak <krzysiek.cisak@gmail.com>
2017-12-10 15:43:25 +0100

    Added Streams

--- LibraryApp/src/utils/LibraryUtils.java
+++ LibraryApp/src/utils/LibraryUtils.java
@@ -1,48 +1,21 @@
 package utils;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 import data.Book;
 import data.Library;
-import data.LibraryUser;
 import data.Magazine;
-import data.Publication;
+
 
 public class LibraryUtils {
 
-	public static void printUsers(Library lib) {
-		// ZMIANA
-		List<LibraryUser> users = new ArrayList<>();
-		users.addAll(lib.getUsers().values());
-		// DODANE
-		Collections.sort(users, new Comparator<LibraryUser>() {
-			@Override
-			public int compare(LibraryUser o1, LibraryUser o2) {
-				return o1.getLastName().compareTo(o2.getLastName());
-			}
-		});
-
-		for (LibraryUser u : users) {
-			System.out.println(u);
-		}
-	}
+	 public static void printUsers(Library lib) {
+		        lib.getUsers().values().stream()
+		                .sorted((a, b) -> a.getLastName().compareTo(b.getLastName()))
+		                .forEach(System.out::println);
+		    }
 
 	public static void printBooks(Library lib) {
-		// ZMIANA
-		List<Publication> publications = new ArrayList<>();
-		publications.addAll(lib.getPublications().values());
-		// DODANE
-		Collections.sort(publications, new Library.AlphabeticalComparator());
-		int countBooks = 0;
-		for (Publication p : publications) {
-			if (p instanceof Book) {
-				System.out.println(p);
-				countBooks++;
-			}
-		}
+		long countBooks = lib.getPublications().values().stream().filter(Book.class::isInstance)
+				.sorted(new Library.AlphabeticalComparator()).peek(System.out::println).count();
 
 		if (countBooks == 0) {
 			System.out.println("Brak książek w bibliotece");
@@ -50,22 +23,11 @@ public class LibraryUtils {
 	}
 
 	public static void printMagazines(Library lib) {
-		// ZMIANA
-		List<Publication> publications = new ArrayList<>();
-		publications.addAll(lib.getPublications().values());
-		// DODANE
-		Collections.sort(publications, new Library.AlphabeticalComparator());
-		int countMagazines = 0;
-		for (Publication p : publications) {
-			if (p instanceof Magazine) {
-				System.out.println(p);
-				countMagazines++;
-			}
-		}
+		long countMagazines = lib.getPublications().values().stream().filter(Magazine.class::isInstance)
+				.sorted(new Library.AlphabeticalComparator()).peek(System.out::println).count();
 
 		if (countMagazines == 0) {
 			System.out.println("Brak magazynów w bibliotece");
 		}
 	}
-
 }
